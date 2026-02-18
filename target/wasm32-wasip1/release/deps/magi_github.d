728e755bc882352d34238bf90971749e818517ac
shmor3 <44666181+shmor3@users.noreply.github.com>
2026-02-18 11:17:41 +0000

    Add .gitignore, remove build artifacts from tracking

--- target/wasm32-wasip1/release/deps/magi_github.d
+++
@@ -1,5 +0,0 @@
-/home/dev/magi-github/target/wasm32-wasip1/release/deps/magi_github.d: src/lib.rs
-
-/home/dev/magi-github/target/wasm32-wasip1/release/deps/magi_github.wasm: src/lib.rs
-
-src/lib.rs:
--- target/wasm32-wasip1/release/magi_github.d
+++
@@ -1,1 +0,0 @@
-/home/dev/magi-github/target/wasm32-wasip1/release/magi_github.wasm: /home/dev/magi/magi-pdk/src/guest.rs /home/dev/magi/magi-pdk/src/lib.rs /home/dev/magi-github/src/lib.rs
